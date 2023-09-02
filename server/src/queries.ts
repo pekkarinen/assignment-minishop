ec50006d5daf9f37ff9e1223f687aa2d8ac7dc3f
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-02 23:28:30 +0300

    Filter by customerId, calculate totalSum

--- server/src/queries.ts
+++ server/src/queries.ts
@@ -5,7 +5,12 @@ import { products } from "./productDatabase.js";
 export const queries: QueryResolvers = {
   orders: async (_parent, _args, _context) => {
     // TODO: Calculate sum, only return orders for requested user
-    return orders.filter(order => order.customerId === _args.customerId).map((order) => ({ ...order, totalSum: 0 }));
+    return orders
+      .filter((order) => order.customerId === _args.customerId)
+      .map((order) => ({
+        ...order,
+        totalSum: order.products.reduce((acc, curr) => acc + curr.amount, 0),
+      }));
   },
   products: async (_parent, _args, _context) => {
     return products;
