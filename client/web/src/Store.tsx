e9f61becb884ea082d5f84a1f9e2a30c81d9c7ba
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-02 19:51:46 +0300

    bracket rules

--- client/web/src/Store.tsx
+++ client/web/src/Store.tsx
@@ -23,7 +23,12 @@ export function Store() {
     }
 
     return data.products.map((product) => (
-      <ProductCard key={product.ean} name={product.name} ean={product.ean} price={product.price} />
+      <ProductCard
+        key={product.ean}
+        name={product.name}
+        ean={product.ean}
+        price={product.price}
+      />
     ));
   };
 
