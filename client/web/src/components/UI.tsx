605a3ca59d613f6d8ff161261eff97720070cba1
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-02 23:07:38 +0300

    Button styles

--- client/web/src/components/UI.tsx
+++ client/web/src/components/UI.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-export function Button(props: { text: string; cb: Function }) {
-  return (
-    <button
-      className="store__button"
-      onClick={props.cb()}>
-      {props.text}
-    </button>
-  );
+export function Button(props: { text: string }) {
+  return <button className="store__button">{props.text}</button>;
 }
