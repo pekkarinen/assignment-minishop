9a458afd702059c58ca79311d75be9450c52262d
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-02 23:17:38 +0300

    Add customerId to query

--- server/src/queries.ts
+++ server/src/queries.ts
@@ -5,7 +5,7 @@ import { products } from "./productDatabase.js";
 export const queries: QueryResolvers = {
   orders: async (_parent, _args, _context) => {
     // TODO: Calculate sum, only return orders for requested user
-    return orders.map((order) => ({ ...order, totalSum: 0 }));
+    return orders.filter(order => order.customerId === _args.customerId).map((order) => ({ ...order, totalSum: 0 }));
   },
   products: async (_parent, _args, _context) => {
     return products;
