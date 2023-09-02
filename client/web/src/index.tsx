6d74f3b7780e6faffdde19331de06039a07af7ed
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-02 19:06:39 +0300

    Order imports, code style

--- client/web/src/index.tsx
+++ client/web/src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-
 import { createRoot } from "react-dom/client";
-import { Store } from "./Store";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Orders } from "./Orders";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { Orders } from "./Orders";
+import { Store } from "./Store";
 
 const gqlClient = new ApolloClient({
   uri: "http://localhost:4000",
