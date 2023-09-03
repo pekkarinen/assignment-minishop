c1221cf28ef98be7700895080276b2b8c3af00a6
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-03 23:51:54 +0300

    Use itemTable, totalSum

--- client/web/src/components/Basket.tsx
+++ client/web/src/components/Basket.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { OrderedProduct } from "../../../generated/graphql";
+import { ItemTable } from "./ItemTable";
+
+function totalSum(products: OrderedProduct[]) {
+  const totalSum = products.reduce((acc, curr) => {
+    const price = curr.amount * curr.product.price;
+    return acc + price;
+  }, 0);
+  return Number(totalSum.toFixed(2));
+}
 
 type BasketProps = {
   items: OrderedProduct[];
@@ -8,11 +17,10 @@ type BasketProps = {
 export function Basket(props: BasketProps) {
   return (
     <div className="store__basket">
-      {props.items?.map((item) => (
-        <p>
-          {item.amount} {item.product.name}
-        </p>
-      ))}
+      <ItemTable
+        products={props.items}
+        totalSum={totalSum(props.items)}
+      />
     </div>
   );
 }
