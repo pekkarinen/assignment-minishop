5cb3826a98e01913b0c746d61c8caee12067fbc9
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-09-05 16:00:35 +0300

    Preliminary add to basket functionality

--- client/web/src/components/Basket.tsx
+++ client/web/src/components/Basket.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { OrderedProduct } from "../../../generated/graphql";
+import { gql, useMutation } from "@apollo/client";
+import { OrderButton } from "./UI";
 import { ItemTable } from "./ItemTable";
+
+const createOrderMutation = gql`
+  mutation CreateOrderWithProducts($customerId: String!, $products: [ProductInput!]!) {
+    createOrderWithProducts(customerId: $customerId, products: $products) {
+      success
+      message
+    }
+  }
+`;
 
 function totalSum(products: OrderedProduct[]) {
   const totalSum = products.reduce((acc, curr) => {
@@ -15,12 +26,40 @@ type BasketProps = {
 };
 
 export function Basket(props: BasketProps) {
+  const [sendOrder, { data, loading, error }] = useMutation(createOrderMutation);
+
+  if (loading) return "sending order...";
+  if (error) return `Oh no! ${error.message}`;
+
   return (
     <div className="store__basket">
-      <ItemTable
-        products={props.items}
-        totalSum={totalSum(props.items)}
-      />
+      {data.success ? (
+        <>"order sent"</>
+      ) : (
+        <>
+          <ItemTable
+            products={props.items}
+            totalSum={totalSum(props.items)}
+          />
+
+          <OrderButton
+            clickHandler={() => {
+              const productInputs = props.items.map((item) => ({
+                ean: item.product.ean,
+                amount: item.amount,
+              }));
+              console.log(productInputs);
+              sendOrder({
+                variables: {
+                  customerId: "customer-1",
+                  products: productInputs,
+                },
+              });
+            }}
+            text="Send order"
+          />
+        </>
+      )}
     </div>
   );
 }
